22d10112ed92e692f6763c6fe07732aed2b8bd09
shahriarhaque <shahriar.haque@gmail.com>
2022-01-24 16:15:01 +1100

    Initial Commit

---
+++ SwiftUIBankingApp/SwiftUIBankingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBankingAppApp.swift
+//  SwiftUIBankingApp
+//
+//  Created by Shahriar Haque on 24/1/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBankingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
